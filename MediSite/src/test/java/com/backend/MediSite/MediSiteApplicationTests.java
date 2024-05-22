edf10aacd1fd671832149635ebd30702542f1834
JaimeReyma <124553920+JaimeReyma@users.noreply.github.com>
2024-05-21 21:26:51 -0600

    Add files via upload

---
+++ MediSite/src/test/java/com/backend/MediSite/MediSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.MediSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
